38aa25427fb92db27f45d7a1576d0eea2e38df19
thesaigoneer <165976396+thesaigoneer@users.noreply.github.com>
2025-04-27 14:41:54 +0700

    Update build.sh - Added zsh & fish

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,6 +5,10 @@ set -ouex pipefail
 dnf5 group install -y development-tools
 dnf5 install -y  procps-ng curl file git
 
+dnf5 install -y zsh
+dnf5 install -y zsh-completions
+dnf5 install -y fish
+
 dnf5 remove -y firefox
 dnf5 remove -y nvtop
 dnf5 remove -y cosmic-edit
