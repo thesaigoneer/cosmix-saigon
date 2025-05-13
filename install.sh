da77aed4743365bdaf5c35c1f271a76a6be8773a
thesaigoneer <165976396+thesaigoneer@users.noreply.github.com>
2025-05-13 14:48:07 +0700

    Update install.sh - add 1 flatpak

--- install.sh
+++ install.sh
@@ -26,6 +26,7 @@ if [[ "$answer" =~ ^[Yy]$ ]]; then
   sudo nixos-rebuild switch
 
   # Add flathub and some apps
+  flatpak install flathub com.vivaldi.Vivaldi -y
   flatpak install flathub com.google.Chrome -y
   flatpak install flathub us.zoom.Zoom -y
   flatpak install flathub org.libreoffice.LibreOffice -y
